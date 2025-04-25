399074e8cd9be5b3deb3ccd858c8fccbdf7f4efd
Abhi-321S <221b014@juetguna.in>
2025-04-25 19:41:12 +0530

    Delete LE5/prob1_lab5 directory

--- LE5/prob1_lab5/Whisky.java
+++
@@ -1,8 +0,0 @@
-class Whisky extends Beverage{
-    void addCondiment(){
-         System.out.println("add soda");
-    }
-    void stir(){
-         System.out.println("stir for 30 sec");
-    }
-}
--- LE5/prob1_lab5/Wine.java
+++
@@ -1,6 +0,0 @@
-class Wine extends Beverage {
-   
-    void addCondiment() {
-        System.out.println("Add a few drops of rosewater to wine.");
-    }
-}
